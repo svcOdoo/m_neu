8939f507ed6cf2fad78468d3106f2fd146607239
Andreas Ahlers <andreas.ahlers@buenting.de>
2025-02-17 11:20:30 +0100

    Ort bei Ablage WA-Bahn hinzugefügt

--- axx_picking_commission_workflow/wizards/axx_put_down_wizard.py
+++ axx_picking_commission_workflow/wizards/axx_put_down_wizard.py
@@ -15,6 +15,18 @@ class AxxPutDownWizard(models.TransientModel):
         string="Kunde"
     )
 
+    axx_partner_dest_street = fields.Char(
+        string="Strasse"
+    )
+
+    axx_partner_dest_zip = fields.Char(
+        string="Postleitzahl"
+    )
+
+    axx_partner_dest_city = fields.Char(
+        string="Ort"
+    )
+
     @api.model
     def default_get(self, fields):
         res = super(AxxPutDownWizard, self).default_get(fields)
@@ -23,4 +35,7 @@ class AxxPutDownWizard(models.TransientModel):
             if picking_id.id:
                 res['axx_location_dest_name'] = picking_id.location_dest_id.name
                 res['axx_partner_dest_name'] = picking_id.partner_id.name
+                res['axx_partner_dest_street'] = picking_id.partner_id.street
+                res['axx_partner_dest_zip'] = picking_id.partner_id.zip
+                res['axx_partner_dest_city'] = picking_id.partner_id.city
         return res
